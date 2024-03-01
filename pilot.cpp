0b457c24dcd6d959193cf24542b9759e587341bf
rjldg <rjlimdeguzman@gmail.com>
2024-03-01 21:19:53 +0800

    added a for loop in pilot.cpp

--- pilot.cpp
+++ pilot.cpp
@@ -6,5 +6,13 @@ int main(){
 
     cout << "This is a string of text" << endl;
 
+    int n;
+
+    cout << "Enter a number: "; cin >> n;
+
+    for(int i = 0; i < n; ++i){
+        cout << "This is another string of text" << endl;
+    }
+
     return 0;
 }
